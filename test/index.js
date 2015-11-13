d2f9ddfedf71d8984efa3eddf643565b368a71d5
821760408-sp <821760408.sp@gmail.com>
2015-11-12 21:08:51 -0500

    added sensor data -> csv file functions

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
-
+var fs = require('fs');
+var csvWriter = require('csv-write-stream');
+var writer = csvWriter({ headers: ['x','y','z'], sendHeaders: false });
 var SensorTag = require('sensortag');
-// https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-communication-with-node-js/
-// #Connecting_from_the_Browser_to_the_Node_Program
+// https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-communication-with-node-js/#Connecting_from_the_Browser_to_the_Node_Program
 // https://github.com/ITPNYU/physcomp/blob/master/labs2014/Node%20Serial%20Lab/wsServer.js
 var WebSocketServer = require('ws').Server;
 var SERVER_PORT = 8081;
@@ -30,18 +31,22 @@ SensorTag.discover(function(tag) {
 	// when you disconnect from a tag, exit the program:
 	tag.on('disconnect', function() {
 		console.log('disconnected!');
+    // close csv stream
+    writer.end();
 		process.exit(0);
 	});
 
 	function connectAndSetUpMe() {			// attempt to connect to the
     console.log('connectAndSetUp');
     console.log('id: ' + tag.id + ' type: ' + tag.type);
+    // open csv stream
+    writer.pipe(fs.createWriteStream(tag.id+'-acc-data.csv'));
     tag.connectAndSetUp(setAccelPeriod);		// when you connect and device is setup, call enableAccelMe
   }
 
 	function setAccelPeriod() {	// set period/SR of accelerometer
     console.log('setAccelerometerPeriod');
-    tag.setAccelerometerPeriod(100, enableAccelMe);
+    tag.setAccelerometerPeriod(100, enableAccelMe); // minimum period 100ms
 	}
 
   function enableAccelMe() {		// attempt to enable the accelerometer
@@ -58,16 +63,15 @@ SensorTag.discover(function(tag) {
   // When you get an accelermeter change, print it out:
   function listenForAcc() {
     tag.on('accelerometerChange', function(x, y, z) {
+      // store accelerometer data
+      accData.x = x.toFixed(3);
+      accData.y = y.toFixed(3);
+      accData.z = z.toFixed(3);
+      // console.log('\tx: '+accData.x+' G \ty: '+accData.y+'G \tz: '+accData.z+' G');
+      // write csv data to the stream
+      writer.write([accData.x, accData.y, accData.z]);
       // broadcast data (https://github.com/ITPNYU/physcomp/blob/master/labs2014/Node%20Serial%20Lab/wsServer.js)
-      if (connections.length > 0) {
-        accData.x = x.toFixed(2);
-        accData.y = y.toFixed(2);
-        accData.z = z.toFixed(2);
-        // console.log('\tx: %d G', accData.x);
-        // console.log('\ty: %d G', accData.y);
-        // console.log('\tz: %d G', accData.z);
-        broadcast(accData);
-      }
+      if (connections.length > 0) { broadcast(accData); }
     });
   }
 
