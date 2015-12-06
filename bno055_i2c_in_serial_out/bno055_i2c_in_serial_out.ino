886dfb0df1ea0e211a2656d307d1b0d7f3bb64e0
iannisis <821760408.sp@gmail.com>
2015-12-05 20:56:37 -0500

    modified Arduino code

--- bno055_i2c_in_serial_out/bno055_i2c_in_serial_out.ino
+++ bno055_i2c_in_serial_out/bno055_i2c_in_serial_out.ino
@@ -4,9 +4,17 @@
 #include <utility/imumaths.h>
 
 /* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (20)
+//#define BNO055_SAMPLERATE_DELAY_MS (20)
+#define BNO055_SAMPLERATE_DELAY_MS (50)
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55); // 55 is arbitrary sensor ID
+sensors_event_t event;
+//imu::Vector<3> accel;
+imu::Vector<3> orientation;
+imu::Vector<3> linaccel;
+imu::Vector<3> gyro;
+
+
 unsigned long prev = 0;
 unsigned long now;
 
@@ -24,22 +32,23 @@ void setup() {
   delay(1000);
 
   bno.setExtCrystalUse(true);
+
+  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
 }
 
 void loop() {
-//  now = millis();
+  //  now = millis();
 
   /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
+  //  bno.getEvent(&event);
 
   /* Display the floating point data */
-//  Serial.print("X: ");
-//  Serial.print(event.orientation.x, 4);
-//  Serial.print("\tY: ");
-//  Serial.print(event.orientation.y, 4);
-//  Serial.print("\tZ: ");
-//  Serial.print(event.orientation.z, 4);
+  //  Serial.print("X: ");
+  //  Serial.print(event.orientation.x, 4);
+  //  Serial.print("\tY: ");
+  //  Serial.print(event.orientation.y, 4);
+  //  Serial.print("\tZ: ");
+  //  Serial.print(event.orientation.z, 4);
 
   /* Display some raw data */
   // Possible vector values can be:
@@ -49,23 +58,45 @@ void loop() {
   // - VECTOR_EULER         - degrees
   // - VECTOR_LINEARACCEL   - m/s^2
   // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  //  accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  orientation = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  Serial.print("\tAccel_X: ");
-  Serial.print(accel.x(), 4);
-  Serial.print("\tAccel_Y: ");
-  Serial.print(accel.y(), 4);
-  Serial.print("\tAccel_Z: ");
-  Serial.print(accel.z(), 4);
+  /* [ori_x, ori_y, ori_z, linacc_x, linacc_y, linacc_z, gyro_x, gyro_y, gyro_z] */
+  Serial.print(orientation.x(), 2); Serial.print("\t");
+  Serial.print(orientation.y(), 2); Serial.print("\t");
+  Serial.print(orientation.z(), 2); Serial.print("\t");
+  //  Serial.print(accel.x(), 2); Serial.print("\t");
+  //  Serial.print(accel.y(), 2); Serial.print("\t");
+  //  Serial.print(accel.z(), 2); Serial.print("\t");
+  Serial.print(linaccel.x(), 2); Serial.print("\t");
+  Serial.print(linaccel.y(), 2); Serial.print("\t");
+  Serial.print(linaccel.z(), 2); Serial.print("\t");
+  Serial.print(gyro.x(), 2); Serial.print("\t");
+  Serial.print(gyro.y(), 2); Serial.print("\t");
+  Serial.print(gyro.z(), 2);
+
+  //  /* Display calibration status for each sensor. */
+  //  uint8_t system, gyro, accel, mag = 0;
+  //  bno.getCalibration(&system, &gyro, &accel, &mag);
+  //  Serial.print("CALIBRATION: Sys=");
+  //  Serial.print(system, DEC);
+  //  Serial.print(" Gyro=");
+  //  Serial.print(gyro, DEC);
+  //  Serial.print(" Accel=");
+  //  Serial.print(accel, DEC);
+  //  Serial.print(" Mag=");
+  //  Serial.println(mag, DEC);
 
   Serial.println("");
 
-//  if (now - prev >= 1000) {
-//    Serial.print("time elapsed: ");
-//    Serial.print(now - prev);
-//    Serial.println("");
-//    prev = now;
-//  }
+  //  if (now - prev >= 1000) {
+  //    Serial.print("time elapsed: ");
+  //    Serial.print(now - prev);
+  //    Serial.println("");
+  //    prev = now;
+  //  }
 
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
